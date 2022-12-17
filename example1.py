6f26658f6e65af9123f83d6b239c918871d676d4
pytut123 <120780089+pytut123@users.noreply.github.com>
2022-12-17 15:20:24 +0530

    Add files via upload

---
+++ example1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+def cube(x):
+    return x**3
+
+
+# In[1]:
+
+
+def odd_even(x):
+    if x%2==0:
+        print(f'{x} is even')
+    else:
+        print(f'{x} is odd')
+
+
+# In[3]:
+
+
+def square(x):
+    return x**2
+
